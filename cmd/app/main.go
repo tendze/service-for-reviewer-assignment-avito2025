53cb2e351c38fbb8dffb775622483d4040f95986
tendze <dang.maxim2811@gmail.com>
2025-11-13 18:18:38 +0300

    logger added

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,10 +1,43 @@
 package main
 
 import (
+	"log/slog"
+	"os"
+
 	"dang.z.v.task/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
-	_ = cfg
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("Данг Зуй Ву написал сервис")
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
